dbdf07c80d006bca8a9685a988f95fdc8fe84235
theovilardo <theovilardo@gmail.com>
2024-11-18 00:26:12 -0300

    tests (menos el ej8)

--- TP2/tp2.pl
+++ TP2/tp2.pl
@@ -145,25 +145,59 @@ generarOp(BS, _, leer(B)) :- member(B, BS).                             % si es 
 % Se espera que completen con las subsecciones de tests que crean necesarias, más allá de las puestas en estos ejemplos
 
 cantidadTestsBasicos(10). % Actualizar con la cantidad de tests que entreguen
+% Ejercicio 1
 testBasico(1) :- proceso(computar).
 testBasico(2) :- proceso(secuencia(escribir(1,pepe),escribir(2,pipo))).
 testBasico(3) :- proceso(paralelo(escribir(1,hola),leer(2))).
 testBasico(4) :- proceso(secuencia(paralelo(computar,leer(1)),escribir(2,fin))).
 testBasico(5) :- not(proceso(invalido)).
+% Ejerccio 2
 testBasico(6) :- buffersUsados(escribir(1, hola), [1]).
 testBasico(7) :- buffersUsados(paralelo(escribir(1,hola),escribir(2,chau)), [1,2]).
 testBasico(8) :- buffersUsados(secuencia(escribir(1,hola),escribir(1,chau)), [1]).
 testBasico(9) :- buffersUsados(paralelo(secuencia(escribir(1,hola),leer(2)),escribir(2,chau)), [1,2]).
 testBasico(10) :- buffersUsados(secuencia(paralelo(escribir(1,a),escribir(2,b)),leer(1)), [1,2]).
+
+% Agregar más tests
+cantidadTestsProcesos(8). % Actualizar con la cantidad de tests que entreguen
+% Ejercicio 3
+testProcesos(1) :- intercalar([1,2],[3,4],TS), member(TS,[[1,2,3,4], [1,3,2,4], [1,3,4,2], [3,1,2,4], [3,1,4,2], [3,4,1,2]]).
+testProcesos(2) :- intercalar([],[1,2],[1,2]).
+testProcesos(3) :- intercalar([1,2],[3,4,5],TS), member(TS,[[1,2,3,4,5], [1,3,2,4,5], [1,3,4,2,5], [1,3,4,5,2], [3,1,2,4,5], [3,1,4,2,5], [3,1,4,5,2], [3,4,1,2,5], [3,4,1,5,2], [3,4,5,1,2]]).
+% Ejercicio 4
+testProcesos(4) :- serializar(computar, [computar]).
+testProcesos(5) :- serializar(secuencia(escribir(1,a),leer(1)), [escribir(1,a),leer(1)]).
+testProcesos(6) :- serializar(paralelo(escribir(1,a),escribir(2,b)),TS), member(TS,[[escribir(1,a),escribir(2,b)],[escribir(2,b),escribir(1,a)]]).
+testProcesos(7) :- serializar(secuencia(paralelo(escribir(1,a),escribir(2,b)),leer(1)),TS), member(TS,[[escribir(1,a),escribir(2,b),leer(1)],[escribir(2,b),escribir(1,a),leer(1)]]).
+testProcesos(8) :- serializar(paralelo(leer(1),leer(2)),TS), member(TS,[[leer(1),leer(2)],[leer(2),leer(1)]]).
+
 % Agregar más tests
 
-cantidadTestsProcesos(0). % Actualizar con la cantidad de tests que entreguen
+cantidadTestsBuffers(10). % Actualizar con la cantidad de tests que entreguen
+% Ejercicio 5
+testBuffers(1) :- contenidoBuffer(1,[escribir(1,hola),escribir(1,mundo)],[hola,mundo]).
+testBuffers(2) :- contenidoBuffer(1,[escribir(1,hola),leer(1)],[]).
+testBuffers(3) :- contenidoBuffer(2,[escribir(1,hola),escribir(2,mundo)],[mundo]).
+testBuffers(4) :- contenidoBuffer(1,secuencia(escribir(1,hola),leer(1)),[]).
+testBuffers(5) :- contenidoBuffer(1,paralelo(escribir(1,hola),escribir(1,mundo)),TS), member(TS,[[hola,mundo],[mundo,hola]]).
+% Ejercicio 6
+testBuffers(6) :- contenidoLeido([escribir(1,hola),leer(1)],[hola]).
+testBuffers(7) :- contenidoLeido(paralelo(secuencia(escribir(2,sol),leer(2)), secuencia(escribir(1,agua),leer(1))), TS), member(TS, [[sol,agua], [sol,agua], [agua,sol], [sol,agua], [agua,sol], [agua,sol]]).
+testBuffers(8) :- contenidoLeido(secuencia(escribir(1,hola),leer(1)),[hola]).
+testBuffers(9) :- contenidoLeido(paralelo(secuencia(escribir(1,a),leer(1)), secuencia(escribir(2,b),leer(2))),TS), member(TS,[[a,b],[b,a]]).
+testBuffers(10) :- contenidoLeido(paralelo(escribir(1,a), secuencia(escribir(1,b),leer(1))),TS), member(TS,[[a],[b]]).
+
 % Agregar más tests
 
-cantidadTestsBuffers(0). % Actualizar con la cantidad de tests que entreguen
-% Agregar más tests
-
-cantidadTestsSeguros(0). % Actualizar con la cantidad de tests que entreguen
+cantidadTestsSeguros(6). % Actualizar con la cantidad de tests que entreguen
+% Ejercicio 7
+testSeguros(1) :- esSeguro(secuencia(escribir(1,hola),leer(1))).
+testSeguros(2) :- not(esSeguro(secuencia(leer(1),escribir(1,hola)))).
+testSeguros(3) :- esSeguro(paralelo(escribir(1,hola),escribir(2,mundo))).
+testSeguros(4) :- not(esSeguro(paralelo(escribir(1,hola),leer(1)))).
+testSeguros(5) :- not(esSeguro(secuencia(escribir(1, hola), leer(2)))).
+testSeguros(6) :- esSeguro(secuencia(paralelo(escribir(1,a),escribir(2,b)), paralelo(leer(1),leer(2)))).
+% Ejercicio 8
 % Agregar más tests
 
 
